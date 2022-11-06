03d4d77ad321af5cebf6fe6f7ba90cbc2ba8182e
CodeSteady7 <chairulikhsan2016@gmail.com>
2022-11-06 12:20:35 +0700

    add

--- app.js
+++ app.js
@@ -26,16 +26,20 @@ const URL = `/api/v1`;
 app.use(cors());
 app.options("*", cors());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
+
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
+
   res.setHeader("Access-Control-Allow-Credentials", true);
+
   next();
 });
 // app.use((req, res, next) => {
