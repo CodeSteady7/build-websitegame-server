37d4b20b3b74824bbcf0384d7ab2ca446a589c76
CodeSteady7 <chairulikhsan2016@gmail.com>
2022-11-11 22:51:06 +0700

    updateuser

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -75,8 +75,6 @@ module.exports = {
     const { email, password } = req.body;
 
     const testPlayer = await Player.find();
-    // console.log("req", req.body);
-    // console.log("req player", testPlayer);
 
     Player.findOne({ email: email })
       .then((player) => {
